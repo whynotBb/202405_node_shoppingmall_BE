61952db02e0fe4ceb9aefefb597ba7ae7c1fd590
whynot-byoul <whynot@hubilon.com>
2024-05-20 17:45:26 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); // req.body 가 객체로 인식되도록
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+    .connect(mongoURI, {useNewUrlParser: true})
+    .then(() => console.log('mongoose connected'))
+    .catch((err) => console.log('connection fail', err));
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('server on');
+});
